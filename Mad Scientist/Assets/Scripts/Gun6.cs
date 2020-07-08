ce523a38da2d97ffdde1b02d3db8a600923dc944
e1tero <58477675+e1tero@users.noreply.github.com>
2020-07-09 05:45:19 +0900

    Adding animation of a new scientist
    - Made and connected animations such as: Idle, Death, Walk, Attack, GetHit, Jump
    - Made a new script for a new gun
    - New bullet type added to prefab

---
+++ Mad Scientist/Assets/Scripts/Gun6.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun6 : MonoBehaviour
+{
+    private Transform firstShootPosition;
+    private Transform secondShootPosition;
+    public GameObject bullet;
+    private GameObject player;
+    private bool delay = false;
+    public float delayTime = 1f;
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = GameObject.FindGameObjectWithTag("Player").GetComponent<Animator>();
+        firstShootPosition = GameObject.FindGameObjectWithTag("firstShootPos").GetComponent<Transform>();
+        secondShootPosition = GameObject.FindGameObjectWithTag("secondShootPos").GetComponent<Transform>();
+    }
+    public void Shoot()
+    {
+        anim.SetTrigger("Shoot");
+        Instantiate(bullet, firstShootPosition.position, firstShootPosition.rotation);
+        Instantiate(bullet, secondShootPosition.position, secondShootPosition.rotation);
+        delay = true;
+        StartCoroutine("FiringDelay");
+    }
+
+    private IEnumerator FiringDelay()
+    {
+        yield return new WaitForSeconds(delayTime);
+        delay = false;
+    }
+
+}
